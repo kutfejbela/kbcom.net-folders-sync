9e996bc395491842721191237bb47ea0da0b67b0
root <root@localhost.localdomain>
2018-08-21 15:35:29 +0200

    Backup

--- usr/share/kbcom.net-sync-folders.bash
+++ usr/share/kbcom.net-sync-folders.bash
@@ -1,7 +1,31 @@
 #!/bin/bash
 
-GLOBAL_FOLDER_SOURCE="$1"
-GLOBAL_FOLDER_DESTINATION="$2"
+### Include functions
+
+GLOBAL_FOLDER_SCRIPT=$(/usr/bin/dirname "$0")
+
+if [ ${GLOBAL_FOLDER_SCRIPT:0:1} == "." ]
+then
+ GLOBAL_FOLDER_SCRIPT="$(/bin/pwd)${GLOBAL_FOLDER_SCRIPT:1}"
+elif [ ${GLOBAL_FOLDER_SCRIPT:0:1} != "/" ]
+then
+ GLOBAL_FOLDER_SCRIPT="$(/bin/pwd)/$GLOBAL_FOLDER_SCRIPT"
+fi
+
+source "$GLOBAL_FOLDER_SCRIPT/.kbcom.net-sync-folders.bash"
+
+
+### Begining of execution
+
+CONFIG_ROOTFOLDER_BACKUP="/opt/.probe/backup"
+CONFIG_ROOTFOLDER_SOURCE="/opt/.probe/source"
+CONFIG_ROOTFOLDER_DESTINATION="/opt/.probe/destination"
+
+GLOBAL_ROOTFOLDER_BACKUP=""
+
+backup_checkcreate_backupfolder
+
+echo "..............$GLOBAL_ROOTFOLDER_BACKUP"
 
 #check létezik és nem üres
-scan_folder "$GLOBAL_FOLDER_SOURCE" "$GLOBAL_FOLDER_DESTINATION"
+scan_folder "$CONFIG_ROOTFOLDER_SOURCE" "$CONFIG_ROOTFOLDER_DESTINATION"
